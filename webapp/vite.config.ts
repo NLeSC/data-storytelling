879eec11d3b95474734c7a97ebf0843d92698a00
Ctw <ctw@ctwhome.com>
2025-10-09 20:57:48 +0200

    Configure server port to 4040 in Vite configuration

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -4,5 +4,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	server: {
+		port: 4040
+	},
 	plugins: [tailwindcss(), sveltekit(), devtoolsJson()]
 });
